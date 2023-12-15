16ff0f5cf1caa686d3517016227b42f20d931482
meg <meg@dhcp-10-8-040-227.mobile.reshsg.uci.edu>
2023-12-14 18:54:52 -0800

    week1

---
+++ week1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args){
+        int x; //declare x exists and is an integer
+        x = 0;
+        while (x < 5) {
+            System.out.println(x);
+            x = x + 1;
+        }
+
+    }
+}
---
+++ week1/HelloWeek1.java
@@ -0,0 +1,11 @@
+public class HelloWeek1 {
+    public static void main(String[] args){
+        System.out.println("Hello week1");
+    }
+}
+/*
+1. All code in Java must be part of a class.
+2. We delimit the beginning and end of egments of code with {}
+3. All statements in Java must end in a semi-colon.
+4. For code to run we need public static void main(String[] args)
+* */
